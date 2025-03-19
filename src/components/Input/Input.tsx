9b263226695275a8a2fdfe5e37abe05fa35d7019
edgarmoraes <102760451+edgarmoraes@users.noreply.github.com>
2025-03-19 01:04:29 +0000

    Update Input.tsx

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -22,12 +22,12 @@
  * ```
  */
 
-import React, { useId, useState } from 'react';
+import { useId, useState, forwardRef } from 'react';
 import styles from './Input.module.css';
 import { InputProps } from './Input.types';
 import classNames from 'classnames';
 
-const Input: React.FC<InputProps> = ({ 
+const Input = forwardRef<HTMLInputElement, InputProps>(({ 
   variant = 'default', 
   label, 
   errorMessage, 
@@ -36,34 +36,30 @@ const Input: React.FC<InputProps> = ({
   type = 'text', 
   isLoading = false, 
   ...rest 
-}) => {
-  const id = useId();  // Generates a unique ID to link the label to the input field
-  const [isPasswordVisible, setIsPasswordVisible] = useState(false);  // Manages the visibility state of the password
+}, ref) => {
+  const id = useId();  
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
-  // Toggles the password visibility between 'text' and 'password'
   const togglePasswordVisibility = () => setIsPasswordVisible(!isPasswordVisible);
 
-  // Combines different CSS classes based on the input variant and error state
   const inputClasses = classNames(styles.input, styles[variant], {
     [styles.error]: !!errorMessage,
   });
 
   return (
     <div className={styles.inputContainer} style={style}>
-      {/* Renders a label if provided, linked to the input field via the unique ID */}
       {label && <label htmlFor={id} className={styles.label}>{label}</label>}
 
       <div className={styles.inputWrapper}>
-        {/* Input field with dynamic type and disabled state if loading */}
         <input
           id={id}
           className={inputClasses}
           type={showTogglePassword && type === 'password' ? (isPasswordVisible ? 'text' : 'password') : type}
           disabled={isLoading || rest.disabled}
+          ref={ref} // <-- Agora a ref está sendo passada corretamente
           {...rest}
         />
 
-        {/* Button to toggle password visibility if the input type is password */}
         {showTogglePassword && type === 'password' && (
           <button
             type="button"
@@ -72,7 +68,6 @@ const Input: React.FC<InputProps> = ({
             disabled={isLoading}
             tabIndex={-1}
           >
-            {/* Displays different icons based on password visibility */}
             {isPasswordVisible ? (
               <img src="src/assets/Icons/password/not-visible.svg" alt='not-visible' width={20} />
             ) : (
@@ -82,10 +77,12 @@ const Input: React.FC<InputProps> = ({
         )}
       </div>
 
-      {/* Displays an error message if provided */}
       {errorMessage && <span className={styles.errorMessage}>{errorMessage}</span>}
     </div>
   );
-};
+});
+
+// Define um display name para facilitar debugging
+Input.displayName = "Input";
 
 export default Input;
