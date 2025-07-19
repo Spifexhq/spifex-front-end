f38152fc3be38cecc14a11213b6b5821cdcfc1eb
edgarmoraes <102760451+edgarmoraes@users.noreply.github.com>
2025-07-19 17:02:10 +0100

    update: button v1.3

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -16,7 +16,7 @@
  * ```
  */
 
-export type ButtonVariant = 'primary' | 'secondary' | 'common' | 'outline' | 'danger' | 'link';
+export type ButtonVariant = 'primary' | 'secondary' | 'common' | 'outline' | 'danger' | 'link' | 'cancel';
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   // Defines the button style variant
