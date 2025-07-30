d2cfd1573efcbf90f827ae915136056c646169ef
edgarmoraes <102760451+edgarmoraes@users.noreply.github.com>
2025-07-30 14:53:54 +0100

    Update index.tsx

--- src/layouts/SettingsLayout/index.tsx
+++ src/layouts/SettingsLayout/index.tsx
@@ -5,13 +5,9 @@ import { Outlet, useLocation } from 'react-router-dom';
 import { useAuth } from '@/api';
 import { AuthMiddleware } from '@/middlewares';
 import { SuspenseLoader } from '@/components/Loaders';
-import Navbar from '@/components/Navbar';
-import SidebarSettings from '@/components/Sidebar/SidebarSettings';
-import { useAuthContext } from '@/contexts/useAuthContext';
 
 export const SettingsLayout: FC = () => {
   const { handleInitUser } = useAuth();
-  const { user } = useAuthContext();
   const location = useLocation();
 
   const [loadingAuth, setLoadingAuth] = useState(true);
@@ -36,8 +32,6 @@ export const SettingsLayout: FC = () => {
 
   return (
     <AuthMiddleware>
-      <Navbar />
-      <SidebarSettings userName={user?.name} activeItem={location.pathname.includes('subscription') ? 'subscription-management' : 'personal'} />
       <div className="flex flex-col h-full w-full">
         <div className="relative z-5 flex-1">
           <Suspense fallback={<SuspenseLoader />}>
