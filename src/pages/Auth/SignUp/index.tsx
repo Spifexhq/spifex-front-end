bfc9add3e20ed2cde75701c8a0a0b0fc754a63c0
edgarmoraes <102760451+edgarmoraes@users.noreply.github.com>
2026-02-15 02:10:51 +0000

    Update index.tsx

--- src/pages/Auth/SignUp/index.tsx
+++ src/pages/Auth/SignUp/index.tsx
@@ -372,7 +372,7 @@ const SignUp = () => {
         {/* Logo */}
         <header className="flex items-center justify-between mb-8">
           <a
-            href="https://dashboard.spifex.com"
+            href="https://spifex.com"
             className="inline-flex items-center gap-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-900/10 rounded-lg"
           >
             <img
